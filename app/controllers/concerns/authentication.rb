2abccb702f802ea461d04d013a19546a3cb3f096
arturoduarte <informa.fj@gmail.com>
2025-01-22 11:21:39 -0300

    avoid authentication in test mode

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,7 +2,7 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
-    before_action :require_authentication
+    before_action :require_authentication unless Rails.env.test?
     helper_method :authenticated?
   end
 
